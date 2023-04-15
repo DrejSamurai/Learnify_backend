c7497c7b8794a82ad5fe2b55bd6443e8e6b82439
DrejSamurai <andrej.mishevski@gmail.com>
2023-04-15 12:12:12 +0200

    Initial commit

---
+++ src/main/java/finki/learnify_backend/LearnifyBackendApplication.java
@@ -0,0 +1,13 @@
+package finki.learnify_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnifyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnifyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/finki/learnify_backend/LearnifyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package finki.learnify_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnifyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
